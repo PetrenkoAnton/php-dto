87f07b6ea8b3e1817d3809fc3a57a2b2b29ccd98
antonpetrenko <anton.petrenko@business-class.com>
2022-12-20 18:44:46 +0200

    Added local docker-compose and phpunit

---
+++ tests/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Domain\Airline;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testAssertTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
